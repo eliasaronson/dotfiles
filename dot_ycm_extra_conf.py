8ec59c5ac3f2acfaea30c12068d4b53d7df7265c
Dummy <dummy@intuicell.com>
2023-09-06 16:58:20 +0200

    Added most dotfiles.

---
+++ dot_ycm_extra_conf.py
@@ -0,0 +1,221 @@
+import os
+import os.path
+import fnmatch
+import logging
+import ycm_core
+import re
+import json
+import subprocess
+
+BASE_FLAGS = [
+        '-std=c++17',
+        '-Wall',
+        '-Wextra',
+        '-Werror',
+        '-Wno-long-long',
+        '-Wno-variadic-macros',
+        '-fexceptions',
+        '-ferror-limit=0',
+        '-DNDEBUG',
+        '-x' 'c++',
+        '-x' 'cuda',
+        '-I/usr/lib/',
+        '-I/usr/bin/',
+        '-I/usr/include/',
+        '-march=native'
+        ]
+
+SOURCE_EXTENSIONS = [
+        '.cpp',
+        '.cxx',
+        '.cc',
+        '.c',
+        '.m',
+        '.mm',
+        '.vert',
+        '.frag',
+        '.cu',
+        '.ino'
+        ]
+
+SOURCE_DIRECTORIES = [
+        'src',
+        'lib'
+        ]
+
+HEADER_EXTENSIONS = [
+        '.h',
+        '.hxx',
+        '.hpp',
+        '.hh'
+        ]
+
+HEADER_DIRECTORIES = [
+        'include',
+        'include/spdlog',
+        'lib',
+        '~/Arduino/libraries/',
+        '~/.arduino15/libraries',
+        '~/.arduino15/packages/arduino/hardware/avr/1.8.3/libraries/',
+        '.pioenvs/',
+        '.arduino15/packages/arduino/hardware/avr/1.8.3/cores/arduino/'
+        ]
+
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in HEADER_EXTENSIONS
+
+def GetCompilationInfoForFile(database, filename):
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            # Get info from the source files by replacing the extension.
+            replacement_file = basename + extension
+            if os.path.exists(replacement_file):
+                compilation_info = database.GetCompilationInfoForFile(replacement_file)
+                if compilation_info.compiler_flags_:
+                    return compilation_info
+            # If that wasn't successful, try replacing possible header directory with possible source directories.
+            for header_dir in HEADER_DIRECTORIES:
+                for source_dir in SOURCE_DIRECTORIES:
+                    src_file = replacement_file.replace(header_dir, source_dir)
+                    if os.path.exists(src_file):
+                        compilation_info = database.GetCompilationInfoForFile(src_file)
+                        if compilation_info.compiler_flags_:
+                            return compilation_info
+        return None
+    return database.GetCompilationInfoForFile(filename)
+
+def FindNearest(path, target, build_folder):
+    candidate = os.path.join(path, target)
+    if(os.path.isfile(candidate) or os.path.isdir(candidate)):
+        logging.info("Found nearest " + target + " at " + candidate)
+        return candidate;
+
+    parent = os.path.dirname(os.path.abspath(path));
+    if(parent == path):
+        raise RuntimeError("Could not find " + target);
+
+    if(build_folder):
+        candidate = os.path.join(parent, build_folder, target)
+        if(os.path.isfile(candidate) or os.path.isdir(candidate)):
+            logging.info("Found nearest " + target + " in build folder at " + candidate)
+            return candidate;
+
+    return FindNearest(parent, target, build_folder)
+
+def find_all_jsons_combine(root, target, build_folder):
+    process = subprocess.run(['find', root, '-name', target], stdout=subprocess.PIPE)
+    paths = process.stdout.decode('utf-8')
+
+    file_paths = list(filter(None, paths.split("\n")))
+    logging.info("Found", len(file_paths), "jsons to combine.")
+
+    if len(file_paths) < 1:
+        return "", False
+
+    file = open(file_paths[0])
+    build_cmds = json.load(file)
+
+    for path in file_paths[1:]:
+        file = open(path)
+        build_cmd = json.load(file)
+        build_cmds += build_cmd
+
+    out_path = root + '/' + build_folder + '/' + 'ycm_' + target
+    with open(out_path, "w") as outfile:
+        json.dump(build_cmds, outfile)
+
+    return out_path, True
+
+def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
+    if not working_directory:
+        return list(flags)
+    new_flags = []
+    make_next_absolute = False
+    path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
+    for flag in flags:
+        new_flag = flag
+
+        if make_next_absolute:
+            make_next_absolute = False
+            if not flag.startswith('/'):
+                new_flag = os.path.join(working_directory, flag)
+
+        for path_flag in path_flags:
+            if flag == path_flag:
+                make_next_absolute = True
+                break
+
+            if flag.startswith(path_flag):
+                path = flag[ len(path_flag): ]
+                new_flag = path_flag + os.path.join(working_directory, path)
+                break
+
+        if new_flag:
+            new_flags.append(new_flag)
+    return new_flags
+
+
+def FlagsForClangComplete(root):
+    try:
+        clang_complete_path, status = find_all_jsons_combine(root, target, build_folder)
+        if not status:
+            logging.info("Could not create combined json. Trying find nearest.")
+            clang_complete_path = FindNearest(root, '.clang_complete')
+
+        clang_complete_flags = open(clang_complete_path, 'r').read().splitlines()
+        return clang_complete_flags
+    except:
+        return None
+
+def FlagsForInclude(root):
+    try:
+        include_path = FindNearest(root, 'include')
+        flags = []
+        for dirroot, dirnames, filenames in os.walk(include_path):
+            for dir_path in dirnames:
+                real_path = os.path.join(dirroot, dir_path)
+                flags = flags + ["-I" + real_path]
+        return flags
+    except:
+        return None
+
+def FlagsForCompilationDatabase(root, filename):
+    try:
+        # Last argument of next function is the name of the build folder for
+        # out of source projects
+        compilation_db_path = FindNearest(root, 'compile_commands.json', 'build')
+        compilation_db_dir = os.path.dirname(compilation_db_path)
+        logging.info("Set compilation database directory to " + compilation_db_dir)
+        compilation_db =  ycm_core.CompilationDatabase(compilation_db_dir)
+        if not compilation_db:
+            logging.info("Compilation database file found but unable to load")
+            return None
+        compilation_info = GetCompilationInfoForFile(compilation_db, filename)
+        if not compilation_info:
+            logging.info("No compilation info for " + filename + " in compilation database")
+            return None
+        return MakeRelativePathsInFlagsAbsolute(
+                compilation_info.compiler_flags_,
+                compilation_info.compiler_working_dir_)
+    except:
+        return None
+
+def FlagsForFile(filename):
+    root = os.path.realpath(filename);
+    compilation_db_flags = FlagsForCompilationDatabase(root, filename)
+    if compilation_db_flags:
+        final_flags = compilation_db_flags
+    else:
+        final_flags = BASE_FLAGS
+        clang_flags = FlagsForClangComplete(root)
+        if clang_flags:
+            final_flags = final_flags + clang_flags
+        include_flags = FlagsForInclude(root)
+        if include_flags:
+            final_flags = final_flags + include_flags
+    return {
+            'flags': final_flags,
+            'do_cache': True
+            }
