4c262d1f2fe2337447d6568c496fa91ab74f942f
elias <elias.aronson@gmail.com>
2023-09-24 10:33:58 +0200

    Added setup script for arch based distros.

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Install packages
+sudo pacman -S neovim zsh chezmoi ripgrep zathura nodejs xsel alacritty brightnessctl nitrogen xss-lock python-pip
+
+yay -S asusctl google-chrome python-pynvim i3lock-color julia-bin caffeine
+
+# Switch shell
+chsh -s $(which zsh)
+
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Generate ssh-key
+sh-keygen -t ed25519 -C "elias.aronson@gmail.com"
+ssh-add ~/.ssh/id_ed25519
+cat .ssh/id_ed25519.pub
+
+# chezmoi setup
+chezmoi init git@github.com:eliasaronson/dotfiles.git
+chezmoi apply --verbose
+
+# Github
+~/scripts/gitalias.sh
+
