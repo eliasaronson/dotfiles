5c72c4a2b39560a288160bc5ccd4f1665e7e5966
Elias H Aronsson <elias@intuicell.com>
2024-09-20 11:29:46 +0200

    Formatter config

--- private_dot_config/nvim/lua/init.lua
+++ private_dot_config/nvim/lua/init.lua
@@ -1,139 +1,145 @@
-require'lspconfig'.pyright.setup{}
+require("lspconfig").pyright.setup({})
 -- require'lspconfig'.clangd.setup{}
-require'lspconfig'.julials.setup{}
-require'lspconfig'.lua_ls.setup({})
-require'lspconfig'.cmake.setup{}
+require("lspconfig").julials.setup({})
+require("lspconfig").lua_ls.setup({})
+require("lspconfig").cmake.setup({})
 
 require("neodev").setup({
-    library = { plugins = { "nvim-dap-ui" }, types = true },
+	library = { plugins = { "nvim-dap-ui" }, types = true },
 })
 
+-- Debugger
 require("dapui").setup()
 require("nvim-dap-virtual-text").setup()
 
-local dap = require('dap')
+local dap = require("dap")
 dap.adapters.cppdbg = {
-    id = 'cppdbg',
-    type = 'executable',
-    command = '/home/elias/Downloads/ms-vscode.cpptools-1.22.2@linux-x64/extension/debugAdapters/bin/OpenDebugAD7',
+	id = "cppdbg",
+	type = "executable",
+	command = "/home/elias/Downloads/ms-vscode.cpptools-1.22.2@linux-x64/extension/debugAdapters/bin/OpenDebugAD7",
 }
 
 dap.configurations.cpp = {
-    {
-        name = "Run intui",
-        type = "cppdbg",
-        request = "launch",
-        program = "${workspaceFolder}/build/intui",
-        args = {"-cl", "build/${fileBasenameNoExtension}.js"},
-        stopAtEntry = false,
-        cwd = "${workspaceFolder}/experiments/nursery",
-        externalConsole = false,
-        console = 'integratedTerminal',
-        MIMode = "gdb",
-        setupCommands = {
-            {
-                description = "Enable pretty-printing for gdb",
-                text = "-enable-pretty-printing",
-                ignoreFailures = true
-            },
-            {
-                description = "Set Disassembly Flavor to Intel",
-                text = "-gdb-set disassembly-flavor intel",
-                ignoreFailures = true
-            }
-        },
-        preLaunchTask = "Build dependencies and self"
-    },
-    {
-        name = "Launch file",
-        type = "cppdbg",
-        request = "launch",
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-        stopAtEntry = false,
-        setupCommands = {  
-            { 
-                text = '-enable-pretty-printing',
-                description =  'enable pretty printing',
-                ignoreFailures = false 
-            },
-        },
-    },
-    {
-        name = 'Attach to gdbserver :1234',
-        type = 'cppdbg',
-        request = 'launch',
-        MIMode = 'gdb',
-        miDebuggerServerAddress = 'localhost:1234',
-        miDebuggerPath = '/usr/bin/gdb',
-        cwd = '${workspaceFolder}',
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-    },
+	{
+		name = "Launch file",
+		type = "cppdbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopAtEntry = false,
+		setupCommands = {
+			{
+				text = "-enable-pretty-printing",
+				description = "enable pretty printing",
+				ignoreFailures = false,
+			},
+		},
+	},
+	{
+		name = "Attach to gdbserver :1234",
+		type = "cppdbg",
+		request = "launch",
+		MIMode = "gdb",
+		miDebuggerServerAddress = "localhost:1234",
+		miDebuggerPath = "/usr/bin/gdb",
+		cwd = "${workspaceFolder}",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+	},
 }
-
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
-dap.configurations.typescript = dap.configurations.cpp
 dap.configurations.cuda = dap.configurations.cpp
+require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "typescript" } })
 
 local dapui = require("dapui")
 dap.listeners.before.attach.dapui_config = function()
-    dapui.open()
+	dapui.open()
 end
 dap.listeners.before.launch.dapui_config = function()
-    dapui.open()
+	dapui.open()
 end
 dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close()
+	dapui.close()
 end
 dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close()
+	dapui.close()
 end
 
+-- Syntax highlighting
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
-require'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+	-- List of parsers to ignore installing (or "all")
+	ignore_install = { "javascript" },
 
-    -- List of parsers to ignore installing (or "all")
-    ignore_install = { "javascript" },
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+	highlight = {
+		enable = true,
 
-    highlight = {
-        enable = true,
+		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+		-- the name of the parser)
+		-- list of language that will be disabled
+		disable = { "c", "rust" },
+		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+		disable = function(lang, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
 
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        disable = { "c", "rust" },
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-}
+-- Formatters
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- JavaScript-family
+		javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+		typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+		json = { "biome" },
+		-- C-family
+		cpp = { "clang-format" },
+		cuda = { "clang-format" },
+		c = { "clang-format" },
+		-- Shell scripts
+		sh = { "shfmt" },
+		bash = { "shfmt" },
+		zsh = { "shfmt" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
 
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
