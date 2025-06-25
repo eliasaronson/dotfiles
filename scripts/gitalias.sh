a8ce8b98ac270835ea6e515f1291b22ca0ffc131
elias <elias@intuicell.com>
2025-06-25 14:39:36 +0200

    More git alias

--- scripts/gitalias.sh
+++ scripts/gitalias.sh
@@ -5,5 +5,8 @@ git config --global alias.st status
 git config --global alias.wc whatchanged
 git config --global alias.d pull
 git config --global alias.u push
+git config --global alias.uf push --force-with-lease
 git config --global alias.cp cherry-pick
+git config --global alias.sw show
+git config --global alias.rs restore --staged
 git config --global --replace-all alias.ad 'add -i'
