7cfc4961451e3f4004ea843e15cee04fc64e0acc
elias <elias@intuicell.com>
2025-07-04 10:50:00 +0200

    More git aliases

--- scripts/gitalias.sh
+++ scripts/gitalias.sh
@@ -5,8 +5,12 @@ git config --global alias.st status
 git config --global alias.wc whatchanged
 git config --global alias.d pull
 git config --global alias.u push
-git config --global alias.uf push --force-with-lease
+git config --global --replace-all alias.uf "push --force-with-lease"
 git config --global alias.cp cherry-pick
 git config --global alias.sw show
-git config --global alias.rs restore --staged
+git config --global --replace-all alias.rs "restore --staged"
+git config --global alias.re restore
+git config --global alias.rb rebase
+git config --global --replace-all alias.rbc "rebase --continue"
+git config --global --replace-all alias.rba "rebase --abort"
 git config --global --replace-all alias.ad 'add -i'
