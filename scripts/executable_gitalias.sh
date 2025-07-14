4c2003e3bfd11262f8485f5e32ea7cbabed72da9
Elias H Aronsson <elias@intuicell.com>
2025-07-14 16:18:11 +0200

    More git aliases

--- scripts/executable_gitalias.sh
+++ scripts/executable_gitalias.sh
@@ -9,6 +9,8 @@ git config --global --replace-all alias.cian "commit --amend --no-verify"
 git config --global alias.st status
 git config --global alias.wc whatchanged
 
+git config --global alias.df diff
+
 git config --global alias.d pull
 git config --global alias.u push
 git config --global --replace-all alias.un "push --no-verify"
@@ -27,7 +29,10 @@ git config --global --replace-all alias.rba "rebase --abort"
 
 git config --global alias.a add
 git config --global --replace-all alias.ad 'add -i'
+git config --global --replace-all alias.ap 'add -p'
 git config --global --replace-all alias.au 'add -u'
+git config --global --replace-all alias.ac '!git add -u && git commit'
+git config --global --replace-all alias.acp '!git add -u && git commit && git push'
 
 git config --global alias.sh stash
 git config --global --replace-all alias.shp 'stash push'
