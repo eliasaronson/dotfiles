f86f159cc782a9e26b06e0c603b8809e02d22d6b
Elias H Aronsson <elias@intuicell.com>
2025-07-15 13:56:17 +0200

    feat: wezterm open history in vim

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,5 +1,7 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local io = require("io")
+local os = require("os")
+local act = wezterm.action
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,27 +9,65 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Batman'
+config.color_scheme = "Molokai"
 
 -- window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
 config.check_for_updates = false
 
 config.enable_scroll_bar = true
+
+wezterm.on("trigger-vim-with-scrollback", function(window, pane)
+	-- Retrieve the text from the pane
+	local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+
+	-- Create a temporary file to pass to vim
+	local name = os.tmpname()
+	local f = io.open(name, "w+")
+	f:write(text)
+	f:flush()
+	f:close()
+
+	-- Open a new window running vim and tell it to open the file
+	window:perform_action(
+		act.SpawnCommandInNewWindow({
+			args = { "vim", name },
+		}),
+		pane
+	)
+
+	-- Wait "enough" time for vim to read the file before we remove it.
+	-- The window creation and process spawn are asynchronous wrt. running
+	-- this script and are not awaitable, so we just pick a number.
+	--
+	-- Note: We don't strictly need to remove this file, but it is nice
+	-- to avoid cluttering up the temporary directory.
+	wezterm.sleep_ms(1000)
+	os.remove(name)
+end)
+config.keys = {
+	{
+		key = "E",
+		mods = "CTRL",
+		action = act.EmitEvent("trigger-vim-with-scrollback"),
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
