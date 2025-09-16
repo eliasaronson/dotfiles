826bed61a7113c5cd50593ac337874d256910f15
Elias H Aronsson <elias@intuicell.com>
2025-09-16 14:24:39 +0200

    Added clipfile

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -24,6 +24,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone git@github.com:eliasaronson/clipfile.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/clipfile
 
 # git
 git config --global pull.rebase true
