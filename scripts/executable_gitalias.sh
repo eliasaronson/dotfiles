890720babff6559e83f643da4303b2c93132b12a
Elias H Aronsson <elias@andonlabs.com>
2026-02-15 19:52:29 -0800

    feat: more git alias

--- scripts/executable_gitalias.sh
+++ scripts/executable_gitalias.sh
@@ -5,6 +5,7 @@ git config --global alias.ci commit
 git config --global --replace-all alias.cia "commit --amend"
 git config --global --replace-all alias.cin "commit --no-verify"
 git config --global --replace-all alias.cian "commit --amend --no-verify"
+git config --global --replace-all alias.cip '!git commit && git push'
 
 git config --global alias.st status
 git config --global alias.wc whatchanged
@@ -20,22 +21,31 @@ git config --global alias.cp cherry-pick
 
 git config --global alias.sw show
 
-git config --global --replace-all alias.rs "restore --staged"
+git config --global --replace-all alias.res "restore --staged"
 git config --global alias.re restore
 
 git config --global alias.rb rebase
 git config --global --replace-all alias.rbc "rebase --continue"
 git config --global --replace-all alias.rba "rebase --abort"
-git config --global --replace-all alias.prb "pull origin master --rebase --autostash"
+git config --global --replace-all alias.rbi "rebase -i"
+git config --global --replace-all alias.prb "pull origin main --rebase --autostash"
 
 git config --global alias.a add
 git config --global --replace-all alias.ad 'add -i'
 git config --global --replace-all alias.ap 'add -p'
 git config --global --replace-all alias.au 'add -u'
 git config --global --replace-all alias.ac '!git add -u && git commit'
+git config --global --replace-all alias.auc '!git add -u && git commit'
 git config --global --replace-all alias.acp '!git add -u && git commit && git push'
+git config --global --replace-all alias.aucp '!git add -u && git commit && git push'
 
 git config --global alias.sh stash
 git config --global --replace-all alias.shp 'stash push'
 
 git config --global --add --bool push.autoSetupRemote true
+
+# Settings
+git config --global pull.rebase true
+git config --global merge.conflictstyle zdiff3
+git config --global rerere.enabled true
+git config --global rerere.autoupdate true
